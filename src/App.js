72799eb872f83b51f28fdf17ed810e8472859a3d
ChristopherHarwell <chrisharwell72@gmail.com>
2022-04-13 15:41:59 -0400

    Formats app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import { useState, React } from "react";
+import React from "react";
 import Form from "./components/Form/Form";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-
-
-  return <div className="App">
-          <Form />
-  </div>;
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
